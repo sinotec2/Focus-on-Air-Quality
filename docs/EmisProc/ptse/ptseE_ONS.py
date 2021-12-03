7605587c9a6ef78244c793f5658406e2842ef1f7
Yungchuan Kuang <sinotec2@gmail.com>
2021-12-03 15:18:25 +0800

    Update ptseE_ONS.py

--- docs/EmisProc/ptse/ptseE_ONS.py
+++ docs/EmisProc/ptse/ptseE_ONS.py
@@ -12,14 +12,20 @@ from scipy.io import FortranFile
 from ptse_sub import CORRECT, add_PMS, check_nan, check_landsea, FillNan, WGS_TWD, Elev_YPM
 
 #Main
-hmp=subprocess.check_output('pwd',shell=True).decode('utf8').strip('\n').split('/')[1]
-P='./'
+P=subprocess.check_output('pwd',shell=True).decode('utf8').strip('\n')+'/'
+teds=int(P.split('/')[3][-2:])
+yr=2016+(teds-10)*3
+ndays=365
+if yr%4==0:ndays=366
+s365=set([i*24 for i in range(ndays)])
+nhrs=ndays*24
+
 Hs=0 #cutting height of stacks
 #Input the TEDS csv file
-try:
-  df = read_csv('point.csv', encoding='utf8')
-except:
-  df = read_csv('point.csv')
+#try:
+df = read_csv('point.csv', encoding='big5')
+#except:
+#  df = read_csv('point.csv')
 # check_NOPandSCC(0)
 df = check_nan(df)
 # check and correct the X coordinates for isolated islands
@@ -35,97 +41,64 @@ df['HY1']=[i*j for i,j in zip(df.HD1,df.DY1)]
 #71 factories with CEMS will emit (at ground) when stacks are operating
 fname=P+'point_cems.csv'
 cems=read_csv(fname)
-if 'CP_NO' not in cems.columns:
-  idx=cems[cems.C_NO=='C_NO'].index
-  cems=cems.drop(idx).reset_index(drop=True)
-  cems['SNF']=[i+j+k for i,j,k in zip(cems.SOX,cems.NOX,cems.FLOW)]
-  cems.drop(cems.loc[cems.SNF==0].index,inplace=True)
-  cems=cems.reset_index(drop=True)
+val='SOX PM NOX FLOW X_BLANK1 X_BLANK2'.split()
+nval=len(val)
+if 'CP_NO' not in cems.columns: #pre-process
+  cems=cems.drop(cems.loc[cems.C_NO=='C_NO'].index).reset_index(drop=True)
   cems['CP_NO'] = [i + j for i, j in zip(list(cems['C_NO']), list(cems['NO_S']))]
   cems['PM']=[(i+j)/2 for i,j in zip(cems.SOX,cems.NOX)]
-  val='SOX PM NOX FLOW X_BLANK1 X_BLANK2'.split()
   if max(cems.HOUR)>100:
     cems['MDH']=[int(i*10000+j*100+k/100) for i,j,k in zip(cems.MONTH,cems.DATE,cems.HOUR)]
   else:
     cems['MDH']=[int(i*10000+j*100+k) for i,j,k in zip(cems.MONTH,cems.DATE,cems.HOUR)]
   cems=pivot_table(cems,index=['CP_NO','MDH'],values=val,aggfunc=sum).reset_index()
-  cems=cems.sort_values('MDH',ascending=True).reset_index(drop=True)
-  cems['MD']=[i//100 for i in cems.MDH]
-  #coverage test
-  id365=365
-  if len(cems.loc[(cems.MD==229)])>0:id365=366
-  s365=set([i*24 for i in range(id365)])
-  ih8760=id365*24
-  mdh=list(set(cems.MDH))
-  if len(mdh)!=ih8760:sys.exit('mdh coverage not enough!')
-  mdh.sort()
-  md=list(set(cems.MD))
-  md.sort()
-  cs,cp_no=[],set(cems.CP_NO)
-  for c in cp_no:
-    n=len(cems.loc[cems.CP_NO==c])
-    if n != ih8760: cs.append(c)
-  #fill the hours
-  idc=['CP_NO']
-  cp_no=set(cems.CP_NO)
-  add_cems=DataFrame({})
-  for c in cp_no:
-    cemsp=cems.loc[cems.CP_NO==c].reset_index(drop=True)
-    n=len(cemsp)
-    if n>ih8760:sys.exit('wrong in ih8760 for '+c)
-    if n != ih8760:
-      a=dict(cemsp.loc[0])
-      for ci in val:
-        a[ci]=0.
-      sMDH=set(mdh)-set(cemsp.MDH)
-      ns=len(sMDH)
-      md_c,mdh_c=[s//100 for s in sMDH],[s for s in sMDH]
-      b=cems.loc[:ns-1].reset_index(drop=True)
-      for ci in idc+val:
-        b.loc[:,ci]=a[ci]
-      b.MD,b.MDH=md_c,mdh_c
-      add_cems=add_cems.append(b,ignore_index=True)
-  cems=cems.append(add_cems,ignore_index=True)
+  #cems(df) convert to cemsM(matrix)
+  for MC in ['CP_NO','MDH']:
+    mc=MC.lower()
+    exec(mc+'=list(set(cems.'+MC+'))');exec(mc+'.sort()')
+    exec('n'+MC+'=len('+mc+')')
+    exec('d'+MC+'={'+mc+'[i]:i for i in range(n'+MC+')}')
+    exec('cems["i'+MC+'"]=[d'+MC+'[i] for i in cems.'+MC+']')
+  if len(mdh)!=ndays*24:sys.exit('mdh coverage not enough!')
+  cemsM=np.zeros(shape=(nMDH,nCP_NO,nval))
+  for i in range(nval):
+    cemsM[cems.iMDH[:],cems.iCP_NO[:],i]=cems[val[i]]
+  DD={}
+  for i in range(nval):
+    DD[val[i]]=cemsM[:,:,i].flatten() 
+  DD['MDH']  =[i for i in mdh for j in cp_no]
+  DD['CP_NO']=[j for i in mdh for j in cp_no]
+  cems=DataFrame(DD)
   cems['C_NO']=[i[:8] for i in cems.CP_NO]
+  cems['MD']=[i//100 for i in cems.MDH]
   cems.set_index('CP_NO').to_csv(fname)
-else:
-  #coverage test
-  id365=365
-  if len(cems.loc[(cems.MD==229)])>0:id365=366
-  s365=set([i*24 for i in range(id365)])
-  ih8760=id365*24
-  mdh=list(set(cems.MDH))
-  mdh.sort()
-  md=list(set(cems.MD))
-  md.sort()
 
-pv_cems1=pivot_table(cems,index=['C_NO','MDH'],values='SOX',aggfunc=sum).reset_index()
-pv_cems1['HR']=[i%100 for i in pv_cems1.MDH]
-cems_HROD=DataFrame({'C_NO':list(set(pv_cems1.C_NO))})
+for MC in ['CP_NO','MDH','MD','C_NO']:
+  mc=MC.lower()
+  exec(mc+'=list(set(cems.'+MC+'))');exec(mc+'.sort()')
+  exec('n'+MC+'=len('+mc+')')
+
+#Hour of Day pattern
+cems['HR']=[i%100 for i in cems.MDH]
+pv_cems1=pivot_table(cems,index=['C_NO','HR'],values='SOX',aggfunc=sum).reset_index()
+
+cems_HROD=DataFrame({'C_NO':c_no})
 cems_HROD['SOX_HR_ODER']=0
-for ic in range(len(cems_HROD.C_NO)):
-  c=cems_HROD.C_NO[ic]
-  pv1=pv_cems1.loc[pv_cems1.C_NO==c]
-  pv3=pivot_table(pv1,index=['HR'],values='SOX',aggfunc=sum).reset_index()
-  pv3=pv3.sort_values('SOX',ascending=False).reset_index(drop=True)
-  s=''
-  for i in range(24):
-    s+=str(list(pv3.HR)[i])+' '  
-  cems_HROD.loc[ic,'SOX_HR_ODER']=s
+for ic in cems_HROD.index:
+  pv1=pv_cems1.loc[pv_cems1.C_NO==c_no[ic]]
+  pv3=pv1.sort_values('SOX',ascending=False).reset_index(drop=True)
+  cems_HROD.loc[ic,'SOX_HR_ODER']=''.join(['{:d} '.format(i) for i in pv3.HR])
 #orders for DY1  
 pv_cems2=pivot_table(cems,index=['C_NO','MD'],values='FLOW',aggfunc=sum).reset_index()
 #Indexing is an exhaustive process.
-pv_cems2['MD']=[mdh.index(i*100) for i in pv_cems2.MD] #change the MMDD into sequence among MMDD00's
-cems_DAOD=DataFrame({'C_NO':list(set(pv_cems2.C_NO))})
+iMD=[mdh.index(i*100) for i in pv_cems2.MD] #change the MMDD into index sequence among MMDD00's
+pv_cems2.MD=iMD
+cems_DAOD=DataFrame({'C_NO':c_no})
 cems_DAOD['FLOW_DA_ODER']=0
-for ic in range(len(cems_DAOD.C_NO)):
-  c=cems_DAOD.C_NO[ic]
-  pv1=pv_cems2.loc[pv_cems2.C_NO==c]
+for ic in cems_DAOD.index:
+  pv1=pv_cems2.loc[pv_cems2.C_NO==c_no[ic]]
   pv3=pv1.sort_values('FLOW',ascending=False).reset_index(drop=True)
-  s=''
-  for i in range(len(pv3)):
-    s+=str(list(pv3.MD)[i])+' '
-  cems_DAOD.loc[ic,'FLOW_DA_ODER']=s
+  cems_DAOD.loc[ic,'FLOW_DA_ODER']=''.join(['{:d} '.format(i) for i in pv3.MD])
 
 dfxy=pivot_table(df,index='C_NO',values=['UTM_E','UTM_N'],aggfunc=np.mean).reset_index() 
 
@@ -142,19 +115,16 @@ for spe in [s for s in [sys.argv[1]] if s in BLS]:
   dfV['C_NO']=[i[:8] for i in dfV.CP_NO]
   for c in colT:
     dfV[c]=np.array(dfV[c],dtype=int)
-  a,b=set(dfV.C_NO),set(cems.C_NO) 
-  a,b=list(a),list(b)
-  a.sort()
-  b.sort()
+  a,b=list(set(dfV.C_NO)),list(set(cems.C_NO));a.sort();b.sort()
   ab=[i for i in a if i in b]
   cp=list(set(dfV.CP_NO))
   cp.sort()
-  ons=np.zeros(shape=(len(cp),len(mdh)))#,dtype=int)
+  ons=np.zeros(shape=(len(cp),nMDH))#,dtype=int)
   #other fatories without CEMS, take the nearest one
-  b1=set(b)-set(dfxy.C_NO)
-  c1=[c for c in b if c not in b1 and c in a]
-  cemsX=np.array([list(dfxy.loc[dfxy.C_NO==c,'UTM_E'])[0] for c in b if c not in b1 and c in a])
-  cemsY=np.array([list(dfxy.loc[dfxy.C_NO==c,'UTM_N'])[0] for c in b if c not in b1 and c in a])
+  b1=set(b)-set(dfxy.C_NO) #cems factory but without UTM location
+  c1=[c for c in b if c not in b1 and c in a] #cems plant with X,Y
+  cemsX=np.array([list(dfxy.loc[dfxy.C_NO==c,'UTM_E'])[0] for c in c1])
+  cemsY=np.array([list(dfxy.loc[dfxy.C_NO==c,'UTM_N'])[0] for c in c1])
   #loop for every factories
   for c in [i for i in a if i not in b1]:
     c_cems=c
@@ -164,15 +134,13 @@ for spe in [s for s in [sys.argv[1]] if s in BLS]:
       idx=list(dist).index(min(dist))
       c_cems=c1[idx]
     pv2MD=np.array(list(cems_DAOD.loc[cems_DAOD.C_NO==c_cems,'FLOW_DA_ODER'])[0].split(),dtype=int)
-    if len(pv2MD)<id365:
-      app=list(s365-set(pv2MD))
-      pv2MD=np.array(list(pv2MD)+app)
+    if len(pv2MD)<ndays: pv2MD=np.array(list(pv2MD)+list(s365-set(pv2MD)))
     df_cp=dfV.loc[dfV.C_NO==c].reset_index(drop=True)
     #loop for every NO_S in this factory
     for p in set(df_cp.CP_NO):
       i=cp.index(p)
       if p in set(cems.CP_NO):
-        ons[i,:]=cems.loc[cems.CP_NO==p,c2v[spe]]*ih8760
+        ons[i,:]=cems.loc[cems.CP_NO==p,c2v[spe]]*nhrs
       else:
         dy1=dfV.DY1[i]
         hd1=dfV.HD1[i]

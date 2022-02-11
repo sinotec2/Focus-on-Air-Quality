d6d87a28ee325d330caecca3eab7de597906a33e
Yunchuan Kuang <sinotec2@gmail.com>
2022-02-11 16:32:06 +0800

    Update gen_inp.py

--- PlumeModels/Terrain/gen_inp.py
+++ PlumeModels/Terrain/gen_inp.py
@@ -9,6 +9,7 @@ import sys,os
 from pyproj import Proj
 import rasterio
 from rasterio.transform import Affine
+from cntr_kml import cntr_kml
 
 #read the 20M DTM data TIFF from https://dtm.moi.gov.tw/2020dtm20m/台灣本島及4離島\(龜山島_綠島_蘭嶼_小琉球\).7z
 fname='taiwan2020.tif'
@@ -135,73 +136,7 @@ text_file.close()
 aermap_path='./'
 os.system(aermap_path+'aermap >& isc.out')
 
-# levels size,>10 too thick, <5 too thin
-N = 10
-mxgrd=max([10.,np.max(grid_z2)])
-levels = np.linspace(0, mxgrd, N)
-col = '#00FF0A #3FFF0A #7FFF0A #BFFF0A #FFFF0A #FECC0A #FD990A #FC660A #FB330A #FA000A'.replace('#', '').split()
-if len(col) != N: print ('color scale not right, please redo from http://www.zonums.com/online/color_ramp/')
-aa = '28'  # ''28'~ 40%, '4d' about 75%
-rr, gg, bb = ([i[j:j + 2] for i in col] for j in [0, 2, 4])
-col = [aa + b + g + r for b, g, r in zip(bb, gg, rr)]
-
-# round the values of levels to 1 significant number at least, -2 at least 2 digits
-i = int(np.log10(levels[1])) - 1
-levels = [round(lev, -i) for lev in levels]
-
-#the Cntr method is valid only in previous version of matplotlib
-c = cntr.Cntr(lon, lat, grid_z2)
-# the tolerance to determine points are connected to the boundaries
-tol = 1E-3
-col0 = '4d6ecdcf'
-col_line0 = 'cc2d3939'
-
-
-#writing the KML, see the KML official website
-head1 = '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://earth.google.com/kml/2.2"><Document><name><![CDATA[' + last + ']]></name>'
-st_head = ''
-st_med = '</color><width>1</width></LineStyle><PolyStyle><color>'
-st_tail = '</color></PolyStyle></Style>'
-for i in range(N):
-  st_head += '<Style id="level' + str(i) + '"><LineStyle><color>' + col[i] + st_med + col[i] + st_tail
-head2 = '</styleUrl><Polygon><outerBoundaryIs><LinearRing><tessellate>1</tessellate><coordinates>'
-tail2 = '</coordinates></LinearRing></outerBoundaryIs></Polygon></Placemark>'
-line = [head1 + st_head]
-# repeat for the level lines
-e, w, s, n = np.max(lon), np.min(lon), np.min(lat), np.max(lat)
-for level in levels[:]:
-  nlist = c.trace(level, level, 0)
-  segs = nlist[:len(nlist) // 2]
-  i = levels.index(level)
-  for seg in segs:
-    line.append('<Placemark><name>level:' + str(level) + '</name><styleUrl>#level' + str(i) + head2)
-    leng = -9999
-    for j in range(len(seg[:, 0])):
-      line.append(str(seg[j, 0]) + ',' + str(seg[j, 1]) + ',0 ')
-      if j > 0:
-        leng = max(leng, np.sqrt((seg[j, 0] - seg[j - 1, 0]) ** 2 + (seg[j, 1] - seg[j - 1, 1]) ** 2))
-    leng0 = np.sqrt((seg[j, 0] - seg[0, 0]) ** 2 + (seg[j, 1] - seg[0, 1]) ** 2)
-    ewsn = np.zeros(shape=(4, 2))
-    j = -1
-    # end points not closed, add coner point(s) to close the polygons.
-    if leng0 > leng and leng0 / leng > 5:
-      if abs(seg[j, 0] - e) < tol: ewsn[0, 1] = 1
-      if abs(seg[0, 0] - e) < tol: ewsn[0, 0] = 1
-      if abs(seg[j, 0] - w) < tol: ewsn[1, 1] = 1
-      if abs(seg[0, 0] - w) < tol: ewsn[1, 0] = 1
-      if abs(seg[j, 1] - s) < tol: ewsn[2, 1] = 1
-      if abs(seg[0, 1] - s) < tol: ewsn[2, 0] = 1
-      if abs(seg[j, 1] - n) < tol: ewsn[3, 1] = 1
-      if abs(seg[0, 1] - n) < tol: ewsn[3, 0] = 1
-      if sum(ewsn[1, :] + ewsn[2, :]) == 2: line.append(str(np.min(lon)) + ',' + str(np.min(lat)) + ',0 ')
-      if sum(ewsn[1, :] + ewsn[3, :]) == 2: line.append(str(np.min(lon)) + ',' + str(np.max(lat)) + ',0 ')
-      if sum(ewsn[0, :] + ewsn[3, :]) == 2: line.append(str(np.max(lon)) + ',' + str(np.max(lat)) + ',0 ')
-      if sum(ewsn[0, :] + ewsn[2, :]) == 2: line.append(str(np.max(lon)) + ',' + str(np.min(lat)) + ',0 ')
-    # TODO: when contour pass half of the domain,must add two edge points.
-    line.append(tail2)
-line.append('</Document></kml>')
-with open(fname + '.kml', 'w') as f:
-  [f.write(i) for i in line]
+result=cntr_kml(grid_z2, lon, lat, fname)
 
 # generate the ISC files
 if M>0:

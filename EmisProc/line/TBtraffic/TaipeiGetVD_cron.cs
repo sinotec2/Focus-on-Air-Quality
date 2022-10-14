caf717246d101dc19b7fa0a6fea43e297d6560fc
sinotec2 <sinotec2@gmail.com>
2022-10-14 14:46:40 +0800

    Update TaipeiGetVD_cron.cs

--- EmisProc/line/TBtraffic/TaipeiGetVD_cron.cs
+++ EmisProc/line/TBtraffic/TaipeiGetVD_cron.cs
@@ -5,13 +5,15 @@ A=`date +%M`
 M=$(( $A % 5 ))
 if [ $M = 0 ] ;then
 ymd=`date  --rfc-3339='date'`
+Y=`date +%Y`
+mkdir -p $Y
 
 wget https://tcgbusfs.blob.core.windows.net/blobtisv/GetVDDATA.xml.gz
 gzip -d GetVDDATA.xml.gz 
 mv GetVDDATA.xml GetVDDATA
 
 /cluster/miniconda/bin/python getVD.py
-cat latest>>$ymd
+cat latest>>$Y/$ymd
 rm latest
 rm GetVDDATA*
 date>>nohup.out

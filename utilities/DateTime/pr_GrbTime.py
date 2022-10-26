6073fdd2d5f2f0ca24aac818c28803c3c7de0021
sinotec2 <sinotec2@gmail.com>
2022-10-26 14:19:21 +0800

    Update pr_GrbTime.py

--- utilities/DateTime/pr_GrbTime.py
+++ utilities/DateTime/pr_GrbTime.py
@@ -1,13 +1,12 @@
 #$ cat ~/bin/pr_GrbTime.py
-#!/opt/anaconda3/bin/python
+#!/opt/anaconda3/envs/gribby/bin/python
 import sys
-import netCDF4
-import datetime
+import pygrib
 fname=sys.argv[1]
-nc = netCDF4.Dataset(fname, 'r')
-V=[list(filter(lambda x:nc.variables[x].ndim==j, [i for i in nc.variables])) for j in [1,2,3,4]]
-nt,nchr=(nc.variables[V[1][0]].shape[i] for i in range(2))
-SDATE=[datetime.datetime.strptime(''.join([str(i, encoding='utf-8') for i in list(nc.variables[V[1][0]][t, :])]),\
- '%m/%d/%Y (%H:%M)') for t in range(nt)]
-for t in range(nt):
-  print(t,SDATE[t])
+grbs = pygrib.open(fname)
+dates=list(set([grbs[i].validDate for i in [1,grbs.messages]]))
+n=len(dates)
+if n>1:
+  dates.sort()
+for i in range(n):
+  print(i,dates[i])

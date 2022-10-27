0c4e6ec4cb1da96344a6add99a0b50672d9db085
sinotec2 <sinotec2@gmail.com>
2022-10-27 08:52:50 +0800

    Update pr_GrbTime.py

--- utilities/DateTime/pr_GrbTime.py
+++ utilities/DateTime/pr_GrbTime.py
@@ -4,8 +4,11 @@ import sys
 import pygrib
 fname=sys.argv[1]
 grbs = pygrib.open(fname)
-dates=list(set([grbs[i].validDate for i in [1,grbs.messages]]))
-n=len(dates)
+m=grbs.messages
+if m <=100:
+  dates=list(set([grbs[i].validDate for i in range(1,m+1)]))
+else:
+  dates=list(set([grbs[i].validDate for i in [1,m]]))n=len(dates)
 if n>1:
   dates.sort()
 for i in range(n):

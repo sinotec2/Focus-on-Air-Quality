6af614bc557dcd29526f8c9e29d3dc1d69db59b2
Yungchuan Kuang <sinotec2@gmail.com>
2023-11-22 17:18:30 +0800

    Create proj_class.py

---
+++ utilities/Crawlers/PDF's/proj_class.py
@@ -0,0 +1,18 @@
+import os
+
+with open('proj_calss.html','r') as f:
+    lines=[i for i in f]
+a=[i for i in lines if "value=" in i and 'B' in i]
+b=[i for i in a if '<' in i and '>' in i]
+cat_Bnum=[i.split('"')[1] for i in a]
+cat_CNnam=[i.split('>')[1].split('<')[0] for i in b]
+for i in range(len(b)):
+    dd=cat_Bnum[i]+'_'+cat_CNnam[i]
+    dd=dd.replace('(','▒~H'))replacce()','▒~I')' )
+    os.system('mkdir -p '+dd)
+
+dd={i:j for i,j in zip(cat_Bnum,cat_CNnam)
+with open('proj_class.json','+w',encoding='utf8') as f:
+    json.dump(dd, f)
+with open('proj_class.json','r') as f:
+    bb=json.load(f)

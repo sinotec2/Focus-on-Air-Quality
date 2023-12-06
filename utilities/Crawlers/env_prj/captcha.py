f759cfad58cc07c6a22ade94b9c34293299eff0b
Yungchuan Kuang <sinotec2@gmail.com>
2023-12-06 17:34:44 +0800

    Update captcha.py

--- utilities/Crawlers/env_prj/captcha.py
+++ utilities/Crawlers/env_prj/captcha.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import cv2, os, time, glob
+import cv2, os, time, glob, sys
 import pytesseract
 from PIL import Image
 from pandas import *
+from selenium.webdriver.firefox.options import Options
 
 def get_captcha(i):
 
@@ -15,15 +16,16 @@ def get_captcha(i):
     screenshot = Image.open(screenshot_path)
 
     # 设置裁剪区域的位置和大小
+    # linux firefox
     x = 100  # 距左边界的像素
     y = 750  # 距顶部的像素
     w = 80  # 宽度
     h = 50  # 高度
-    
-    x = 200  # 距左边界的像素
-    y = 1500  # 距顶部的像素
-    w = 150  # 宽度
-    h = 100  # 高度
+# mac Firefox    
+#    x = 200  # 距左边界的像素
+#    y = 1500  # 距顶部的像素
+#    w = 150  # 宽度
+#    h = 100  # 高度
     # 裁剪图像
     cropped_image = screenshot.crop((x, y, x + w, y + h))
 
@@ -38,51 +40,84 @@ def get_captcha(i):
     print(CaptchaCode,len(CaptchaCode))
     return CaptchaCode
 
+def print_single_line(msg):
+    sys.stdout.write('\033[F')  # 光标移动到上一行
+    sys.stdout.write('\033[K')  # 清除行
+    print(msg)
+
 df0=read_csv('df0.csv')
 df0=df0.loc[df0.yr_mg>=110].reset_index(drop=True)
 
 # 创建 Chrome 驱动器
-driver = webdriver.Firefox()
-source_directory="/Users/kuang/Downloads"
+source_directory="/home/kuang/Downloads"
 
-for i in range(len(df0)):
+#firefox_options = Options()
+#firefox_options.add_argument("--headless")  
+options = webdriver.FirefoxOptions()
+
+# 禁用內建的 PDF 開啟器
+options.set_preference("pdfjs.disabled", True)
+
+# 禁用內建的 PDF 下載器
+#options.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
+
+j=1
+for i in range(600):#len(df0)):
     # 打开网页
-    proj_id=df0.proj_id[i]
+    proj_id=str(df0.proj_id[i])
     cat_nam=df0.cat_nam[i]
-    title=df0.title[i]
-    group_id=df0.group_id[i]
-    target_directory="/nas2/ses_pub/EPAReports/"+group_id+"_"+cat_nam
-    os.makedirs(os.path.expanduser(target_directory))
+    title=df0.title[i].replace(' ','').replace('(','\(').replace(')','\)')
+    group_id=str(df0.group_id[i])
+    target_directory="/nas2/sespub/EPA_PrjReports/"+group_id+"_"+cat_nam
+    os.makedirs(os.path.expanduser(target_directory), exist_ok=True)
+    tfile=os.path.join(os.path.expanduser(target_directory), proj_id+"_"+title+".pdf")
+    if os.path.exists(tfile):continue
 
+    driver = webdriver.Firefox(options=options)
     url = "https://epq.moenv.gov.tw/ProjectDoc/FileDownload?proj_id="+proj_id+"&group_id="+group_id
 
-    i=0
-    while i<10:
-        i+=1
+    ii=0
+    pdf_files = glob.glob(os.path.join(os.path.expanduser(source_directory), "*.pdf"))
+    if len(pdf_files)>1:
+        sfile=os.path.join(os.path.expanduser(source_directory), '*.pdf')
+        os.system("rm "+sfile)
+
+    while ii<10:
+        ii+=1
         driver.get(url)
         driver.save_screenshot("./screenshot.png")
-        CaptchaCode=get_captcha(i)
+        CaptchaCode=get_captcha(ii)
         if len(CaptchaCode)!=4:continue
-        if len(set(list(CaptchaCode))-set(list('/?,.!@#$%^&*()|')+['\n']+["'"]+['"'])) != len(CaptchaCode):continue
+        if len(set(list(CaptchaCode))-set(list('/?,.!@#$%^&*()[]|')+['\n']+["'"]+['"'])) != len(CaptchaCode):continue
         # 输入验证码（如果需要）
         captcha_input = driver.find_element(By.ID, "CaptchaCode")
-        print(i,CaptchaCode)
+        print_single_line('{:d} '.format(ii)+CaptchaCode+title)
         captcha_input.send_keys(CaptchaCode)
         time.sleep(3)
         # 点击 "我同意"
         agree_button = driver.find_element(By.XPATH, "//input[@value='我同意']")
         agree_button.click()
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 30)
         try:
             ok_button = driver.find_element(By.XPATH, "//button[text()='OK']")
             # 点击 OK 按钮
             ok_button.click()
+            sys.exit()
             continue
         except:
-            pdf_files = glob.glob(os.path.join(os.path.expanduser(source_directory), "*.pdf"))
+            pdf_files=[]
+            iii=0
+            while iii<10 and len(pdf_files)==0:
+                wait = WebDriverWait(driver, 30)
+                
+                pdf_files = glob.glob(os.path.join(os.path.expanduser(source_directory), "*.pdf"))
+                iii+=1
+            if len(pdf_files)==0:
+                driver.quit()    # 关闭浏览器
+                break
+            pdf_files[0]=pdf_files[0].replace('(','\(').replace(')','\)').replace(' ','\ ')
             sfile=os.path.join(os.path.expanduser(source_directory), pdf_files[0])
-            tfile=os.path.join(os.path.expanduser(target_directory), proj_id+"_"+title+".pdf")
             os.system("mv "+sfile+" "+tfile)
+            driver.quit()    # 关闭浏览器
             break
-    # 关闭浏览器
-driver.quit()
+    if j==1:break
